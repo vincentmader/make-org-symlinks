127792b1ab768adcf0bf4d80f2c1fe9a4999528a
Vincent C. Mader <vincent.mader@icloud.com>
2023-10-27 13:50:36 +0200

    FIX Update paths to org directories

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 
 PATH_TO_HOME = Path.home()
-PATH_TO_ORG = Path(PATH_TO_HOME, "org")
-PATH_TO_ALL = Path(PATH_TO_ORG, "A_all")
+PATH_TO_ORG = Path(PATH_TO_HOME, "org", "project")
+PATH_TO_ALL = Path(PATH_TO_HOME, "org", "library", "links", "project", "all")
 
 IS_VERBOSE = False
